d1cd87f0696789cb1ca3b417a56054bd963909b8
Frankacer <bflloyd1653@gmail.com>
2023-06-21 14:47:07 -0700

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,13 @@
  * print_name - prints a name
  * @name: name string to be printed
  * @f: pointer to print function
+ *
+ * This function takes a name and a function pointer as arguments.
+ * The function pointed to by @f is responsible for printing the
+ * name in a specific format. The provided @name is passed as an
+ * argument to the function pointed by @f, resulting in the name
+ * being printed according to the specific function's
+ * implementation.
  */
 void print_name(char *name, void (*f)(char *))
 {

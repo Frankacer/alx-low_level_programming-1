28a0f70cceeebfc25ccc750542f0bc05be9e5ef4
Frankacer <bflloyd1653@gmail.com>
2023-06-20 17:10:17 -0700

    My first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - models a dog object
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: This struct stores the attributes of a dog
+ */
+struct dog {
+	char * name;
+	float age;
+	char * owner;
+};
+
+#endif

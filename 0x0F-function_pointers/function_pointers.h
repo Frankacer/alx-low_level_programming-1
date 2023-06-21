74ddb32a84ffb825bd91a2a61644bdcf39f365fe
Frankacer <bflloyd1653@gmail.com>
2023-06-21 15:24:52 -0700

    fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 #ifndef NULL
 #define NULL ((void *)0)
 #endif /*NULL*/
 
+#include <stddef.h>
+
+void print_elem(int elem);
+void print_elem_hex(int elem);
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name_uppercase(char *name);
 void print_name_as_is(char *name);
 void print_name(char *name, void (*f)(char *));

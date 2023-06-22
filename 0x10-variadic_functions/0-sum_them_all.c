c4f22f499776a3e432b8075ecce3c74defea6227
Frankacer <bflloyd1653@gmail.com>
2023-06-22 15:06:19 -0700

    fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,15 +15,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	sum = 0;
-	va_start(args, n);
 
 	if (n == 0)
 		return (0);
 
+	va_start(args, n);
 	for (i = 0; i <= n; i++)
 		sum += va_arg(args, int);
-	
-	va_end(args);	
+
+	va_end(args)
 	return (sum);
 }
 

49630158978f484fd6e44c3557b4d4ece6c83542
Frankacer <bflloyd1653@gmail.com>
2023-06-22 15:56:04 -0700

    fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_numbers - Prints numbers followed by a new line.
@@ -22,12 +24,8 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	/* Function implementation */
-}
-
-void print_numbers(const char *separator, const unsigned int n, ...)
-{
 	va_list args;
+	unsigned int i;
 
 	if (separator == NULL)
 		return;

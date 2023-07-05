a59967d69166dda84fe48aba97a6d8dd60bc7675
Frankacer <bflloyd1653@gmail.com>
2023-07-05 14:01:43 -0700

    My first commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints a singly linked list
+ * @h: pointer to linked list
+ *
+ * Return: returns number of nodes in list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	/*Traverse list and increment count*/
+	while (h != NULL)
+	{
+		printf("%d/n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a project on singly-linked lists but a step further from where we stopped the last time.

e85f0a90b3aaad2969623e664c761a1af2bd6686
Frankacer <bflloyd1653@gmail.com>
2023-07-06 18:13:23 -0700

    fix

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t list.
+ * @head: pointer to head of listint_t list
+ * @index: index of node starting at 0
+ *
+ * Return: the node at index, otherwise NULL.
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return (NULL);
+
+	/*Allocate memory for node and check success*/
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+
+	/*Traverse list*/
+	unsigned int i = 0;
+
+	while (head && i <= index)
+	{
+		if (i == index)
+		{
+			node = head;
+			return (node);
+		}
+		head = head->next;
+		i++;
+	}
+}
+
